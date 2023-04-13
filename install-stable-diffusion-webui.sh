d4c06acb63e2de7e69deb88380ff9320b2ccdc15
User <user@ex12465879.com>
2023-04-13 12:15:32 +0700

    Checkout 0cc0ee1

--- install-stable-diffusion-webui.sh
+++ install-stable-diffusion-webui.sh
@@ -30,8 +30,8 @@ cd /kaggle/tmp
 git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui $QUIET
 echo -e "${INFO_COLOR}    Downgrading Stable Diffusion to a working release${NO_COLOR}"
 cd /kaggle/tmp/stable-diffusion-webui
-git checkout 22bcc7be428c94e9408f589966c2040187245d81
-#git checkout 0cc0ee1
+#git checkout 22bcc7be428c94e9408f589966c2040187245d81
+git checkout 0cc0ee1
 
 
 # FastAPI
