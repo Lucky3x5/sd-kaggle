71e18e80c731833a4c44bda8a0b684af5ebc99fd
User <user@ex12465879.com>
2023-04-15 17:29:40 +0700

    Persistence test

--- install-stable-diffusion-webui.sh
+++ install-stable-diffusion-webui.sh
@@ -9,8 +9,18 @@ NO_COLOR='\033[0m'
 NONPERSISTENT_DIR="/kaggle/tmp"
 PERSISTENT_DIR="/kaggle/working"
 
+if [ "x$USE_PERSISTENCE" = "x1" ]; then
+    SDW_DIR=$PERSISTENT_DIR/stable-diffusion-webui
+else
+    SDW_DIR=$NONPERSISTENT_DIR/stable-diffusion-webui
+fi
+
+CNET_MODEL_DIR=$NONPERSISTENT_DIR/controlnet-models
+MODEL_DIR=$NONPERSISTENT_DIR/stable-diffusion-models
+EMB_DIR=$NONPERSISTENT_DIR/stable-diffusion-embeddings
+
 if [ "x$FORCE_REINSTALL" = "x1" ]; then
-    rm -rf $NONPERSISTENT_DIR/.memfix $NONPERSISTENT_DIR/repositories $PERSISTENT_DIR/stable-diffusion-webui
+    rm -rf $NONPERSISTENT_DIR/.memfix $MODEL_DIR $CNET_MODEL_DIR $EMB_DIR $SDW_DIR
 fi
 
 python --version
@@ -35,26 +45,28 @@ fi
 
 # Stable Diffusion WebUI
 echo -e "${INFO_COLOR}Installing Stable Diffusion WebUI${NO_COLOR}"
-cd /kaggle/tmp
-if [ ! -d "/kaggle/tmp/stable-diffusion-webui" ]; then
-echo -e "${INFO_COLOR}    Installing Stable Diffusion WebUI Core${NO_COLOR}"
-git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui $QUIET
+if [ ! -d "$SDW_DIR" ]; then
+    echo -e "${INFO_COLOR}    Installing Stable Diffusion WebUI Core${NO_COLOR}"
+    git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui $SDW_DIR $QUIET
+    mv $SDW_DIR/models $MODEL_DIR
+    ln -s $MODEL_DIR $SDW_DIR/models
 fi
 echo -e "${INFO_COLOR}    Downgrading Stable Diffusion to a working release${NO_COLOR}"
-cd /kaggle/tmp/stable-diffusion-webui
+cd $SDW_DIR
 git fetch
 #git checkout 22bcc7be428c94e9408f589966c2040187245d81
 git checkout 0cc0ee1
 
+
 # Python VENV
-if [ ! -d "/kaggle/tmp/stable-diffusion-webui/venv/bin" ]; then
+if [ ! -d "$SDW_DIR/venv/bin" ]; then
 echo -e "${INFO_COLOR}Creating Python virtual environment${NO_COLOR}"
-python -m venv /kaggle/tmp/stable-diffusion-webui/venv
+python -m venv $SDW_DIR/venv
 fi
 echo -e "${INFO_COLOR}Activating Python virtual environment${NO_COLOR}"
-source /kaggle/tmp/stable-diffusion-webui/venv/bin/activate
+source $SDW_DIR/venv/bin/activate
 echo -e "${INFO_COLOR}Upgrading Python virtual environment tools${NO_COLOR}"
-pip install --upgrade pip setuptools
+pip install --upgrade pip setuptools $QUIET
 
 # FastAPI
 echo -e "${INFO_COLOR}Installing FastAPI${NO_COLOR}"
@@ -65,56 +77,52 @@ echo -e "${INFO_COLOR}Installing pyTorch${NO_COLOR}"
 pip install torch==1.13.1+cu117 torchvision==0.14.1+cu117 torchaudio==0.13.1 torchtext==0.14.1 torchdata==0.5.1 --extra-index-url https://download.pytorch.org/whl/cu117 -U $QUIET
 
 if [ "x$INSTALL_CONTROLNET" = "x1" ]; then
+    echo -e "${INFO_COLOR}    Installing Stable Diffusion WebUI ControlNet extension${NO_COLOR}"
+    if [ ! -d "$SDW_DIR/extensions/sd-webui-controlnet" ]; then 
+        git clone https://github.com/Mikubill/sd-webui-controlnet $SDW_DIR/extensions/sd-webui-controlnet $QUIET
+    else
+        cd $SDW_DIR/extensions/sd-webui-controlnet
+        git pull $QUIET
+    fi
 
-echo -e "${INFO_COLOR}    Installing Stable Diffusion WebUI ControlNet extension${NO_COLOR}"
-	
-if [ ! -d "/kaggle/tmp/stable-diffusion-webui/extensions/sd-webui-controlnet" ]; then 
-git clone https://github.com/Mikubill/sd-webui-controlnet /kaggle/tmp/stable-diffusion-webui/extensions/sd-webui-controlnet $QUIET
-else
-cd /kaggle/tmp/stable-diffusion-webui/extensions/sd-webui-controlnet
-git pull $QUIET
-fi
-
-
-echo -e "${INFO_COLOR}    Installing Stable Diffusion WebUI OpenPose editor extension${NO_COLOR}"
-if [ ! -d "/kaggle/tmp/stable-diffusion-webui/extensions/openpose-editor" ]; then 
-git clone https://github.com/fkunn1326/openpose-editor /kaggle/tmp/stable-diffusion-webui/extensions/openpose-editor $QUIET
-else
-cd /kaggle/tmp/stable-diffusion-webui/extensions/openpose-editor
-git pull $QUIET
-fi
-
+    echo -e "${INFO_COLOR}    Installing Stable Diffusion WebUI OpenPose editor extension${NO_COLOR}"
+    if [ ! -d "$SDW_DIR/extensions/openpose-editor" ]; then 
+        git clone https://github.com/fkunn1326/openpose-editor $SDW_DIR/extensions/openpose-editor $QUIET
+    else
+        cd $SDW_DIR/extensions/openpose-editor
+        git pull $QUIET
+    fi
 fi
 
 echo -e "${INFO_COLOR}    Installing Stable Diffusion WebUI Image Browser extension${NO_COLOR}"
-if [ ! -d "/kaggle/tmp/stable-diffusion-webui/extensions/stable-diffusion-webui-images-browser" ]; then 
-git clone https://github.com/yfszzx/stable-diffusion-webui-images-browser /kaggle/tmp/stable-diffusion-webui/extensions/stable-diffusion-webui-images-browser $QUIET
+if [ ! -d "$SDW_DIR/extensions/stable-diffusion-webui-images-browser" ]; then 
+git clone https://github.com/yfszzx/stable-diffusion-webui-images-browser $SDW_DIR/extensions/stable-diffusion-webui-images-browser $QUIET
 else
-cd /kaggle/tmp/stable-diffusion-webui/extensions/stable-diffusion-webui-images-browser
+cd $SDW_DIR/extensions/stable-diffusion-webui-images-browser
 git pull $QUIET
 fi
 
 echo -e "${INFO_COLOR}    Installing Stable Diffusion WebUI LoCon extension${NO_COLOR}"
-if [ ! -d "/kaggle/tmp/stable-diffusion-webui/extensions/a1111-sd-webui-locon" ]; then 
-git clone https://github.com/Lucky3x5/a1111-sd-webui-locon /kaggle/tmp/stable-diffusion-webui/extensions/a1111-sd-webui-locon $QUIET
+if [ ! -d "$SDW_DIR/extensions/a1111-sd-webui-locon" ]; then 
+git clone https://github.com/Lucky3x5/a1111-sd-webui-locon $SDW_DIR/extensions/a1111-sd-webui-locon $QUIET
 else
-cd /kaggle/tmp/stable-diffusion-webui/extensions/a1111-sd-webui-locon
+cd $SDW_DIR/extensions/a1111-sd-webui-locon
 git pull $QUIET
 fi
 
 echo -e "${INFO_COLOR}    Installing Stable Diffusion WebUI LoRA block weight extension${NO_COLOR}"
-if [ ! -d "/kaggle/tmp/stable-diffusion-webui/extensions/sd-webui-lora-block-weight" ]; then 
-git clone https://github.com/hako-mikan/sd-webui-lora-block-weight /kaggle/tmp/stable-diffusion-webui/extensions/sd-webui-lora-block-weight $QUIET
+if [ ! -d "$SDW_DIR/extensions/sd-webui-lora-block-weight" ]; then 
+git clone https://github.com/hako-mikan/sd-webui-lora-block-weight $SDW_DIR/extensions/sd-webui-lora-block-weight $QUIET
 else
-cd /kaggle/tmp/stable-diffusion-webui/extensions/sd-webui-lora-block-weight
+cd $SDW_DIR/extensions/sd-webui-lora-block-weight
 git pull $QUIET
 fi
 
 echo -e "${INFO_COLOR}    Installing Stable Diffusion WebUI HighRes Fix extension${NO_COLOR}"
-if [ ! -d "/kaggle/tmp/stable-diffusion-webui/extensions/stable-diffusion-webui-hires-fix-progressive" ]; then 
-git clone https://github.com/Kahsolt/stable-diffusion-webui-hires-fix-progressive /kaggle/tmp/stable-diffusion-webui/extensions/stable-diffusion-webui-hires-fix-progressive $QUIET
+if [ ! -d "$SDW_DIR/extensions/stable-diffusion-webui-hires-fix-progressive" ]; then 
+git clone https://github.com/Kahsolt/stable-diffusion-webui-hires-fix-progressive $SDW_DIR/extensions/stable-diffusion-webui-hires-fix-progressive $QUIET
 else
-cd /kaggle/tmp/stable-diffusion-webui/extensions/stable-diffusion-webui-hires-fix-progressive
+cd $SDW_DIR/extensions/stable-diffusion-webui-hires-fix-progressive
 git pull $QUIET
 fi
 
@@ -125,76 +133,62 @@ read -ra newarr <<< "$SD_MODELS"
 for model_url in "${newarr[@]}";
 do
     model_name=$(basename "$model_url")
-    if [ ! -r "/kaggle/tmp/stable-diffusion-webui/models/Stable-diffusion/$model_name" ]; then
+    if [ ! -r "$MODEL_DIR/Stable-diffusion/$model_name" ]; then
     echo -e "${INFO_COLOR}            $model_name${NO_COLOR}"
-    curl -Lo "/kaggle/tmp/stable-diffusion-webui/models/Stable-diffusion/$model_name" "$model_url"
+    curl -Lo "$MODEL_DIR/Stable-diffusion/$model_name" "$model_url"
     fi
 done
 
-if [ "x$INSTALL_CONTROLNET" = "x1" ]; then
 ## ControlNet Models
-echo -e "${INFO_COLOR}    Installing ControlNet Models${NO_COLOR}"
-#curl -Lo /kaggle/tmp/stable-diffusion-webui/extensions/sd-webui-controlnet/models/control_canny.safetensors https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/control_canny-fp16.safetensors
-#curl -Lo /kaggle/tmp/stable-diffusion-webui/extensions/sd-webui-controlnet/models/control_depth.safetensors https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/control_depth-fp16.safetensors
-#curl -Lo /kaggle/tmp/stable-diffusion-webui/extensions/sd-webui-controlnet/models/control_hed-fp16.safetensors https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/control_hed-fp16.safetensors
-#curl -Lo /kaggle/tmp/stable-diffusion-webui/extensions/sd-webui-controlnet/models/control_mlsd-fp16.safetensors https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/control_mlsd-fp16.safetensors
-#curl -Lo /kaggle/tmp/stable-diffusion-webui/extensions/sd-webui-controlnet/models/control_normal-fp16.safetensors https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/control_normal-fp16.safetensors
-#curl -Lo /kaggle/tmp/stable-diffusion-webui/extensions/sd-webui-controlnet/models/control_openpose-fp16.safetensors https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/control_openpose-fp16.safetensors
-#curl -Lo /kaggle/tmp/stable-diffusion-webui/extensions/sd-webui-controlnet/models/control_scribble-fp16.safetensors https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/control_scribble-fp16.safetensors
-#curl -Lo /kaggle/tmp/stable-diffusion-webui/extensions/sd-webui-controlnet/models/control_seg-fp16.safetensors https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/control_seg-fp16.safetensors
-#curl -Lo /kaggle/tmp/stable-diffusion-webui/extensions/sd-webui-controlnet/models/t2iadapter_canny_sd14v1.pth https://huggingface.co/TencentARC/T2I-Adapter/resolve/main/models/t2iadapter_canny_sd14v1.pth
-#curl -Lo /kaggle/tmp/stable-diffusion-webui/extensions/sd-webui-controlnet/models/t2iadapter_color_sd14v1.pth https://huggingface.co/TencentARC/T2I-Adapter/resolve/main/models/t2iadapter_color_sd14v1.pth
-#curl -Lo /kaggle/tmp/stable-diffusion-webui/extensions/sd-webui-controlnet/models/t2iadapter_depth_sd14v1.pth https://huggingface.co/TencentARC/T2I-Adapter/resolve/main/models/t2iadapter_depth_sd14v1.pth
-#curl -Lo /kaggle/tmp/stable-diffusion-webui/extensions/sd-webui-controlnet/models/t2iadapter_keypose_sd14v1.pth https://huggingface.co/TencentARC/T2I-Adapter/resolve/main/models/t2iadapter_keypose_sd14v1.pth
-#curl -Lo /kaggle/tmp/stable-diffusion-webui/extensions/sd-webui-controlnet/models/t2iadapter_openpose_sd14v1.pth https://huggingface.co/TencentARC/T2I-Adapter/resolve/main/models/t2iadapter_openpose_sd14v1.pth
-#curl -Lo /kaggle/tmp/stable-diffusion-webui/extensions/sd-webui-controlnet/models/t2iadapter_seg_sd14v1.pth https://huggingface.co/TencentARC/T2I-Adapter/resolve/main/models/t2iadapter_seg_sd14v1.pth
-#curl -Lo /kaggle/tmp/stable-diffusion-webui/extensions/sd-webui-controlnet/models/t2iadapter_sketch_sd14v1.pth https://huggingface.co/TencentARC/T2I-Adapter/resolve/main/models/t2iadapter_sketch_sd14v1.pth
-#curl -Lo /kaggle/tmp/stable-diffusion-webui/extensions/sd-webui-controlnet/models/t2iadapter_style_sd14v1.pth https://huggingface.co/TencentARC/T2I-Adapter/resolve/main/models/t2iadapter_style_sd14v1.pth
-if [ ! -d "/kaggle/tmp/stable-diffusion-webui/extensions/sd-webui-controlnet/models/.git" ]; then
-rm -rf /kaggle/tmp/stable-diffusion-webui/extensions/sd-webui-controlnet/models
-git clone https://huggingface.co/webui/ControlNet-modules-safetensors /kaggle/tmp/stable-diffusion-webui/extensions/sd-webui-controlnet/models
-fi
-cd /kaggle/tmp/stable-diffusion-webui/extensions/sd-webui-controlnet/models
-git pull $QUIET
-git lfs pull $QUIET
+if [ "x$INSTALL_CONTROLNET" = "x1" ]; then
+    echo -e "${INFO_COLOR}    Installing ControlNet Models${NO_COLOR}"
+    if [ ! -d "$SDW_DIR/extensions/sd-webui-controlnet/models/.git" ]; then
+        git clone https://huggingface.co/webui/ControlNet-modules-safetensors $CNET_MODEL_DIR
+        rm -rf $SDW_DIR/extensions/sd-webui-controlnet/models
+        ln -s $CNET_MODEL_DIR $SDW_DIR/extensions/sd-webui-controlnet/models
+    fi
+    cd $CNET_MODEL_DIR
+    git pull $QUIET
+    git lfs pull $QUIET
 fi
 
 ## LoRA
 echo -e "${INFO_COLOR}    Installing Stable Diffusion LoRAs${NO_COLOR}"
-if [ ! -d "/kaggle/tmp/stable-diffusion-webui/models/Lora/.git" ]; then
-rm -rf /kaggle/tmp/stable-diffusion-webui/models/Lora
-git clone https://huggingface.co/Lucky555/Lora /kaggle/tmp/stable-diffusion-webui/models/Lora $QUIET
+if [ ! -d "$MODEL_DIR/Lora/.git" ]; then
+    rm -rf $MODEL_DIR/Lora
+    git clone https://huggingface.co/Lucky555/Lora $MODEL_DIR/Lora $QUIET
 fi
-cd /kaggle/tmp/stable-diffusion-webui/models/Lora
+cd $MODEL_DIR/Lora
 git pull $QUIET
 git lfs pull $QUIET
 
 ## Embeddings
 echo -e "${INFO_COLOR}    Installing Stable Diffusion Embeddings${NO_COLOR}"
-if [ ! -d "/kaggle/tmp/stable-diffusion-webui/embeddings/.git" ]; then
-rm -rf /kaggle/tmp/stable-diffusion-webui/embeddings
-git clone https://huggingface.co/Lucky555/embeddings /kaggle/tmp/stable-diffusion-webui/embeddings $QUIET
+if [ ! -d "$SDW_DIR/embeddings/.git" ]; then
+    git clone https://huggingface.co/Lucky555/embeddings $EMB_DIR/embeddings $QUIET
+    rm -rf $SDW_DIR/embeddings
+    ln -s $EMB_DIR $SDW_DIR/embeddings
 fi
-cd /kaggle/tmp/stable-diffusion-webui/embeddings
+cd $EMB_DIR
 git pull $QUIET
 git lfs pull $QUIET
 
 ## VAE
 echo -e "${INFO_COLOR}    Installing Stable Diffusion VAEs${NO_COLOR}"
-if [ ! -d "/kaggle/tmp/stable-diffusion-webui/models/VAE/.git" ]; then
-rm -rf /kaggle/tmp/stable-diffusion-webui/models/VAE
-git clone https://huggingface.co/Lucky555/VAE /kaggle/tmp/stable-diffusion-webui/models/VAE $QUIET
+if [ ! -d "$MODEL_DIR/VAE/.git" ]; then
+    rm -rf $MODEL_DIR/VAE
+    git clone https://huggingface.co/Lucky555/VAE $MODEL_DIR/VAE $QUIET
 fi
-cd /kaggle/tmp/stable-diffusion-webui/models/VAE
+cd $MODEL_DIR/VAE
 git pull $QUIET
 git lfs pull $QUIET
 
 ## ESRGAN
 echo -e "${INFO_COLOR}   Installing Stable Diffusion ESRGAN${NO_COLOR}"
-if [ ! -d "/kaggle/tmp/stable-diffusion-webui/models/ESRGAN" ]; then
-git clone https://huggingface.co/nolanaatama/ESRGAN /kaggle/tmp/stable-diffusion-webui/models/ESRGAN $QUIET
+if [ ! -d "$MODEL_DIR/ESRGAN" ]; then
+    git clone https://huggingface.co/nolanaatama/ESRGAN $MODEL_DIR/ESRGAN $QUIET
 fi
-cd /kaggle/tmp/stable-diffusion-webui/models/ESRGAN
+cd $MODEL_DIR/ESRGAN
 git pull $QUIET
 git lfs pull $QUIET
 
